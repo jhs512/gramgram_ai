c357dd681f6dc852a7618d86113d35ea4ba01723
jhs512 <jangka512@gmail.com>
2023-03-27 19:34:06 +0900

    세팅

---
+++ src/main/java/com/ll/gramgram_ai/GramgramAiApplication.java
@@ -0,0 +1,13 @@
+package com.ll.gramgram_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GramgramAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GramgramAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/gramgram_ai/GramgramAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.gramgram_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GramgramAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
